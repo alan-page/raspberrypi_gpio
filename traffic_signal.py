7ada9e8f6c5cd4c53f72eb803cfb85092cc42627
Alan Page <alandouglaspage@gmail.com>
2022-06-27 17:53:19 -0400

    More GPIO pin abstraction; less hard-coding

--- traffic_signal.py
+++ traffic_signal.py
@@ -6,9 +6,18 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 #GPIO.setwarnings(False)
+
+# Red
 GPIO.setup(2,GPIO.OUT)
+red = 2
+
+# Yellow
 GPIO.setup(3,GPIO.OUT)
+yellow = 3
+
+# Green
 GPIO.setup(4,GPIO.OUT)
+green = 4
 
 # Fraction or multiple of seconds for each ON state in the loop (1 = 10 seconds of red light)
 timefactor = 1
@@ -16,29 +25,29 @@ timefactor = 1
 try:
     while True:
         print "Red"
-        GPIO.output(2,GPIO.HIGH)
-        GPIO.output(3,GPIO.LOW)
+        GPIO.output(yellow,GPIO.LOW)
+        GPIO.output(red,GPIO.HIGH)
         durationR = timefactor * 10
         time.sleep(durationR)
 
         print "Green"
-        GPIO.output(2,GPIO.LOW)
-        GPIO.output(4,GPIO.HIGH)
+        GPIO.output(red,GPIO.LOW)
+        GPIO.output(green,GPIO.HIGH)
         durationG = timefactor * 10
         time.sleep(durationG)
 
         print "Yellow"
-        GPIO.output(4,GPIO.LOW)
-        GPIO.output(3,GPIO.HIGH)
+        GPIO.output(green,GPIO.LOW)
+        GPIO.output(yellow,GPIO.HIGH)
         durationY = timefactor * 4
         time.sleep(durationY)
 
 except KeyboardInterrupt:
     # Special Ctrl-C code
     print "...Ctrl-C..."
-    GPIO.output(2,GPIO.LOW)
-    GPIO.output(3,GPIO.LOW)
-    GPIO.output(4,GPIO.LOW)
+    GPIO.output(red,GPIO.LOW)
+    GPIO.output(yellow,GPIO.LOW)
+    GPIO.output(green,GPIO.LOW)
 
 except:
     # Other interrupts (errors maybe?)
@@ -47,9 +56,9 @@ except:
 finally:
     # Turn off the lights
     #   ...this  doesn't appear work...
-    GPIO.output(2,GPIO.LOW)
-    GPIO.output(3,GPIO.LOW)
-    GPIO.output(4,GPIO.LOW)
+    GPIO.output(red,GPIO.LOW)
+    GPIO.output(yellow,GPIO.LOW)
+    GPIO.output(green,GPIO.LOW)
 
     # Cleanup GPIO pins on program exit
     GPIO.cleanup()
