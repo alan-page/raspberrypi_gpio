c9798a5e1b6ae65b039b1ae4112428b4e8c39e04
Alan Page <alandouglaspage@gmail.com>
2022-10-06 20:21:37 -0400

    Adjusted final temp value by 1

--- keyes_sensors/ky-001/thermometer.py
+++ keyes_sensors/ky-001/thermometer.py
@@ -75,8 +75,8 @@ def TemperaturAuswertung():
 try:
     while True:
         temp_value = TemperaturAuswertung()
-        # Cast string of "xx.y" value to a float, then an integer
-        temp_value_int = int(float(temp_value))
+        # Cast string of "xx.y" value to a float, then an integer (and then add 1)
+        temp_value_int = int(float(temp_value)) + 1
         temp_bar_string = ""
         for t in range(0, temp_value_int):
             temp_bar_string += "="
